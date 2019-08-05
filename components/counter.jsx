d9a2755fef2084ea0098ceaf3b53a45f8bf7b20e
mkirwa <mahlonkibiwott@gmail.com>
2019-08-04 19:30:03 -0600

    Add files via upload

--- components/counter.jsx
+++ components/counter.jsx
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 //component class has methods that will be inherited in counter class
 class Counter extends Component {
     state = {
-        count:0,
+        count:0, tags:['tag1','tag2','tag3']
     };
     
+    renderTags(){
+        if(this.state.tags.length===0) return <p>There are no tags</p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
+
+
     render() { 
         //includes any data that this component needs 
         //it could have an address, a street property or anything this component needs 
+        //const classes includes all the data that we will pass in span
+        //start with primary classes that we want to have in all cases
+        //this render method is about determing the class for this label
+        
         return(
             <div>
-                <span className = "badge badge-warning m-2">{this.formatCount()}</span>
-                <button className = "btn btn-secondary btn-sm" >Increment</button>
+                {this.renderTags()}
             </div>
             );
     }
-    //adding methods
-    formatCount(){
-        const {count} = this.state;
-        return count === 0 ? "Zero":count;
-    }
+    
 }
  
 export default Counter;

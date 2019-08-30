9418d285663fde40d7ac314116cf2b971db644bd
mkirwa <mahlonkibiwott@gmail.com>
2019-08-29 20:14:20 -0600

    Add files via upload

--- allcomponents.jsx
+++ allcomponents.jsx
@@ -2,9 +2,26 @@ import React, {Component}from 'react';
 import Counter from './counter';
 
 class Allcomponents extends Component {
-    state = {  }
+    state = { 
+        //instead of hard coding all the counters in the render method
+        //div section, 
+        //just use an array instead
+        // Use this to uniquely identify each
+        //counter
+        allcomponents:[
+            {}
+        ]
+     };
     render() { 
-        return ( <div></div> );
+        return ( 
+        //this div is the wrapper or the container for allcomponents
+        <div>
+            <Counter />
+            <Counter />
+            <Counter />
+            <Counter />
+        </div> 
+        );
     }
 }
  

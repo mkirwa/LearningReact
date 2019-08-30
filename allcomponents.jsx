e4310d124791f6c7eb7cdc575fd84d3173dcbba9
mkirwa <mahlonkibiwott@gmail.com>
2019-08-29 19:36:02 -0600

    Add files via upload

---
+++ allcomponents.jsx
@@ -0,0 +1,11 @@
+import React, {Component}from 'react';
+import Counter from './counter';
+
+class Allcomponents extends Component {
+    state = {  }
+    render() { 
+        return ( <div></div> );
+    }
+}
+ 
+export default Allcomponents;

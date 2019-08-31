141c46e74f46976f54221c57caf2fe016b0c9ae5
mkirwa <mahlonkibiwott@gmail.com>
2019-08-30 21:09:16 -0600

    Add files via upload

--- counter.jsx
+++ counter.jsx
@@ -19,15 +19,16 @@ class Counter extends Component {
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button 
                 onClick={() => this.handleIncrement({id:1})} 
-                className="btn btn-secondary btn-sm">
+                className="btn btn-secondary btn-sm.">
                     Increment
                 </button>
+                <button onClick={this.handleDelete} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
             );
     }
     
     getBadgeClasses() {
-        let classes = "badge m2 badge-";
+        let classes = "badge m-2 badge-";
         classes += this.state.value === 0? "warning" : "primary";
         return classes; 
     }

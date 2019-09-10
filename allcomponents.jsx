de902640d421a44d373ad8cc181c48fae7167897
mkirwa <mahlonkibiwott@gmail.com>
2019-09-09 21:08:47 -0600

    Add files via upload

--- allcomponents.jsx
+++ allcomponents.jsx
@@ -22,7 +22,39 @@ class Allcomponents extends Component {
             { id:4, value:0 },
         ]
      };
-     
+
+    handleIncrement = counter =>{
+        //creating a new counters array and giving it
+        //to the set state method and have react update 
+        //the state
+        //... this is the spread operator clones 
+        //this.state.allcomponents
+        //with these, you will get a new array of counters
+        //the object in the new array(counters) are the same
+        //objects we have in allcomponents
+        const counters = [...this.state.allcomponents]
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({allcomponents: counters});
+    };
+    
+    handleReset = () =>{
+        //this.state.allcomponents - this refers to the existing
+        //counters 
+        //we use the map method to get each counter and reset
+        //its value to zero and then return it
+        //this give you a new array of counters
+        //You store them in counters. 
+        //
+        const counters = this.state.allcomponents.map(c=>{
+            c.value = 0;
+            return c;
+        });
+        //here you call this.setState with this new array
+        this.setState({ allcomponents: counters})
+    };
+
     handleDelete = (counterId)=>{
         const counters = this.state.allcomponents.filter(c => c.id !==counterId);
         this.setState({ allcomponents: counters})
@@ -37,10 +69,14 @@ class Allcomponents extends Component {
         //key will not be part of that because it is a special attribute
         //for uniquely identify elements
         <div>
+            <button 
+            onClick={this.handleReset}
+            className="btn btn-primary btn-sm m-2">Reset</button>
             {this.state.allcomponents.map(counter => 
             <Counter 
                 key={counter.id} 
                 onDelete={this.handleDelete} 
+                onIncrement={this.handleIncrement}
                 counter={counter}
                 />
                 

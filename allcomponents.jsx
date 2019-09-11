e437e79c03543131ba836e7f5f3b7f74f5bd2e0c
mkirwa <mahlonkibiwott@gmail.com>
2019-09-10 20:02:17 -0600

    Add files via upload

--- allcomponents.jsx
+++ allcomponents.jsx
@@ -6,7 +6,7 @@ import Counter from './counter';
 class Allcomponents extends Component {
     render() { 
         //Destructuring arguments
-
+        const {onReset, counters, onDelete, onIncrement} = this.props;
         return ( 
         //this div is the wrapper or the container for allcomponents
         //counter -- get each counter and map it to a counter component
@@ -16,13 +16,13 @@ class Allcomponents extends Component {
         //for uniquely identify elements
         <div>
             <button 
-            onClick={this.props.onReset}
+            onClick={onReset}
             className="btn btn-primary btn-sm m-2">Reset</button>
-            {this.props.counters.map(counter => 
+            {counters.map(counter => 
             <Counter 
                 key={counter.id} 
-                onDelete={this.props.onDelete} 
-                onIncrement={this.props.onIncrement}
+                onDelete={onDelete} 
+                onIncrement={onIncrement}
                 counter={counter}
                 />
                 

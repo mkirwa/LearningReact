78fd30b6f022fba0f41c6e064a85b895a600b71e
mkirwa <mahlonkibiwott@gmail.com>
2019-09-13 21:44:31 -0600

    Add files via upload

---
+++ search.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    state = {  }
+    render() { 
+        return ( 
+        <div>
+            
+
+        </div>
+        );
+    }
+}
+ 
+export default Search;

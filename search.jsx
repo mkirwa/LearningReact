a1943409caf0d1c6736672b6864564fb5a21b1e9
mkirwa <mahlonkibiwott@gmail.com>
2019-09-14 21:21:42 -0600

    Add files via upload

--- search.jsx
+++ search.jsx
@@ -16,6 +16,63 @@ class Search extends Component {
         passwordError: ""
 
     }
+
+    change = e => {
+        // this.props.onChange({ [e.target.name]: e.target.value });
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+      };
+
+      validate = () => {
+        let isError = false;
+        const errors = {
+          firstNameError: "",
+          lastNameError: "",
+          usernameError: "",
+          emailError: "",
+          passwordError: ""
+        };
+    
+        if (this.state.username.length < 5) {
+          isError = true;
+          errors.usernameError = "Username needs to be atleast 5 characters long";
+        }
+    
+        if (this.state.email.indexOf("@") === -1) {
+          isError = true;
+          errors.emailError = "Requires valid email";
+        }
+    
+        this.setState({
+          ...this.state,
+          ...errors
+        });
+    
+        return isError;
+      };
+    
+      onSubmit = e => {
+        e.preventDefault();
+        const err = this.validate();
+        if (!err) {
+          this.props.onSubmit(this.state);
+          // clear form
+          this.setState({
+            firstName: "",
+            firstNameError: "",
+            lastName: "",
+            lastNameError: "",
+            username: "",
+            usernameError: "",
+            email: "",
+            emailError: "",
+            password: "",
+            passwordError: ""
+          });
+        }
+      };
+
     render() { 
         return ( 
         <div>

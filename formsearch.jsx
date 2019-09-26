ae9779ff56044ea9c49c98112baf0e43f7d56133
mkirwa <mahlonkibiwott@gmail.com>
2019-09-25 20:19:50 -0600

    Add files via upload

--- formsearch.jsx
+++ formsearch.jsx
@@ -1,35 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class formsearch extends Component {
-   constructor(props){
-       super(props)
-       this.state={
-        fullName:null
-       }
-   }
-   //this is where you run your API calls
-    handleSubmit = (event) =>{
-        event.preventDefault()
-        const data = this.state
-    }
+  //This is where you run your API calls to firebase
 
-    handleInputChange = (event) =>{
-        event.preventDefault()
-        this.setState({[event.target.name]:event.target.value})
-    }
-    render() { 
-        const {search} = this.state
-        return ( 
-        <div>
-            <form onSubmit={this.handleSubmit} class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="search" aria-label="search" name='fullName' onChange="this.handleInputChange">
-                    <div pseudo="placeholder">Search</div>
-                    <div contentEditable="plaintext-only"></div>
-                </input>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-         );
-    }
-} 
+  render() {
+    //this is where const {search } = this.state; was
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit} class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="text"
+            placeholder="search"
+            aria-label="search"
+            name="fullName"
+            onChange="this.handleInputChange"
+          >
+            <div pseudo="placeholder">Search</div>
+            <div contentEditable="plaintext-only"></div>
+          </input>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 export default formsearch;

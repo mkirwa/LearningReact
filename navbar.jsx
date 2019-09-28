9c8e0b10f93601670f801ef971634ef421e6536c
mkirwa <mahlonkibiwott@gmail.com>
2019-09-28 16:09:36 -0600

    Add files via upload

--- navbar.jsx
+++ navbar.jsx
@@ -22,7 +22,7 @@ class NavBar extends Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
+            <li className="nav-item active">
               <a className="nav-link" href="#">
                 Loads<span className="sr-only">(current)</span>
               </a>
@@ -33,7 +33,7 @@ class NavBar extends Component {
               </a>
             </li>
 
-            <li className="dropdown-menu" id="dropdown">
+            <li className="nav-item dropdown">
               <a
                 className="nav-link dropdown-toggle"
                 href="#"

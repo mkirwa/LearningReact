d27e2bb8a2fabce6087e15d69bae51eff4700453
mkirwa <mahlonkibiwott@gmail.com>
2019-09-29 16:38:47 -0600

    Add files via upload

---
+++ loads.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Loads extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>This is the loads page</h1>
+      </div>
+    );
+  }
+}
+
+export default Loads;

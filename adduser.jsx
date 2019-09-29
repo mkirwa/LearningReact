1ee71abd29a1ad112a9d761efdced24a8485f7e4
mkirwa <mahlonkibiwott@gmail.com>
2019-09-29 16:39:29 -0600

    Add files via upload

---
+++ adduser.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Adduser extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>This is the add user page</h1>
+      </div>
+    );
+  }
+}
+
+export default Adduser;
---
+++ dispatch.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Dispatch extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>This is the dispatch page</h1>
+      </div>
+    );
+  }
+}
+
+export default Dispatch;
